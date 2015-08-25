9bb403cd87385c5481af603b0c4e49b8bd51fcd2
李俊杰 <phpor@users.noreply.github.com>
2015-08-25 15:14:15 +0800

    Create string.bat

---
+++ bat/string.bat
@@ -0,0 +1,15 @@
+@echo off
+set ifo=abcdefghijklmnopqrstuvwxyz0123456789
+echo 原字符串（第二行为各字符的序号）：
+echo %ifo%
+echo 123456789012345678901234567890123456
+echo 截取前5个字符：
+echo %ifo:~0,5%
+echo 截取最后5个字符：
+echo %ifo:~-5%
+echo 截取第一个到倒数第6个字符：
+echo %ifo:~0,-5%
+echo 从第4个字符开始，截取5个字符：
+echo %ifo:~3,5%
+echo 从倒数第14个字符开始，截取5个字符：
+echo %ifo:~-14,5%
